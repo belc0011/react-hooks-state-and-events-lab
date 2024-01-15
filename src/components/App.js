323cad943373fade5a854b6d559383fbabc6cf86
Monica Garcia <monicairenegarcia@gmail.com>
2024-01-15 14:06:10 -0700

    completed state and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
+  const [darkMode, setToggleMode] = useState(true)
+  
   const appClass = false ? "App dark" : "App light"
-
+  function handleClick() {
+    setToggleMode(() => !darkMode)
+  }
+    const backgroundColor = darkMode ? "white" : "black"
+    const textColor = darkMode ? "black" : "white"
   return (
-    <div className={appClass}>
+    <div className={darkMode ? "App light" : "App dark"}>
       <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <h2 style={{color: textColor}}>Shopster</h2>
+        <button onClick={handleClick}>{darkMode ? "Dark Mode" : "Light Mode"}</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={itemData} color={backgroundColor}/>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Item({ name, category }) {
+function Item({ name, category, style }) {
+  const [inCart, setInCart] = useState(true)
+  function handleClick() {
+    setInCart((inCart) => !inCart)
+  }
   return (
-    <li className="">
+    <li className={inCart ? "" : "in-cart"} style={style}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{inCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
